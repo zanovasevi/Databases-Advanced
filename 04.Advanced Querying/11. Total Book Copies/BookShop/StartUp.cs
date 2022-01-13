20c22503947beb8f5e1ed97b480ab26c8f79eae4
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 14:48:31 +0200

    Add files via upload

---
+++ 04.Advanced Querying/11. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,214 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            //int year = int.Parse(Console.ReadLine());
+            //string command = Console.ReadLine();
+            //string date = Console.ReadLine();
+            //string input = Console.ReadLine().ToLower();
+            //int lengthCheck = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountCopiesByAuthor(db));
+        }
+
+        // задача 4 + int year = int.Parse(Console.ReadLine());
+
+        //public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        //{
+        //    var books = context.Books
+        //        .Where(x => x.ReleaseDate.Value.Year != year)
+        //        .Select(x => new
+        //        {
+        //            x.BookId,
+        //            x.Title
+        //        })
+        //        .OrderBy(x => x.BookId)
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach (var book in books)
+        //    {
+        //        sb.AppendLine(book.Title);
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 5
+
+        //public static string GetBooksByCategory(BookShopContext context, string input)
+        //{
+        //    var categories = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        //    var books = context.Books
+        //        .Where(x => x.BookCategories
+        //        .Any(x => categories.Contains(x.Category.Name)))
+        //        .Select(x => new
+        //        {
+        //            x.Title
+        //        })
+        //        .OrderBy(x => x.Title)
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach (var book in books)
+        //    {
+        //        sb.AppendLine(book.Title);
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 6
+
+        //public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        //{
+        //    var targetDate = DateTime.ParseExact(date, "dd-MM-yyyy",
+        //        CultureInfo.InvariantCulture);
+
+        //    var books = context.Books
+        //        .Where(x => x.ReleaseDate.Value < targetDate)
+        //        .Select(x => new
+        //        {
+        //            x.Title,
+        //            x.EditionType,
+        //            x.Price,
+        //            x.ReleaseDate
+        //        })
+        //        .OrderByDescending(x => x.ReleaseDate)
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach (var book in books)
+        //    {
+        //        sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:F2}");
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 7
+
+        //public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        //{
+        //    var names = context.Authors
+        //        .Where(x => x.FirstName.EndsWith(input))
+        //        .Select(x => new
+        //        {
+        //            FullName = x.FirstName + " " + x.LastName
+        //        })
+        //        .OrderBy(x => x.FullName)
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach (var name in names)
+        //    {
+        //        sb.AppendLine(name.FullName);
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 8 - Book Search
+
+        //public static string GetBookTitlesContaining(BookShopContext context, string input)
+        //{
+        //    var books = context.Books
+        //        .Where(x => x.Title.Contains(input))
+        //        .Select(x => new
+        //        {
+        //            x.Title
+        //        })
+        //        .OrderBy(x => x.Title)
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach (var book in books)
+        //    {
+        //        sb.AppendLine(book.Title);
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 9
+
+        //public static string GetBooksByAuthor(BookShopContext context, string input)
+        //{
+        //    var books = context.Books
+        //        .Where(x => EF.Functions.Like(x.Author.LastName, $"{input}%"))
+        //        .OrderBy(x => x.BookId)
+        //        .Select(x => new
+        //        {
+        //            x.BookId,
+        //            x.Title,
+        //            FullName = x.Author.FirstName + " " + x.Author.LastName
+        //        })
+        //        .ToList();
+
+        //    var sb = new StringBuilder();
+
+        //    foreach(var book in books)
+        //    {
+        //        sb.AppendLine($"{book.Title} ({book.FullName})");
+        //    }
+
+        //    return sb.ToString().TrimEnd();
+        //}
+
+        // задача 10
+
+        //public static int CountBooks(BookShopContext context, int lengthCheck)
+        //{
+        //    var books = context.Books
+        //        .Where(x => x.Title.Length > lengthCheck)
+        //        .Select(x => x.Title)
+        //        .ToArray();
+
+        //    return books.Count();
+        //}
+
+        // задача 11
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var authors = context.Authors
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    TotalCopies = x.Books.Sum(x => x.Copies)
+                })
+                .OrderByDescending(x => x.TotalCopies)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach(var au in authors)
+            {
+                sb.AppendLine($"{au.FirstName} {au.LastName} - {au.TotalCopies}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
