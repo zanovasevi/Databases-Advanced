ec630d4f9652a08ea5db6ff6dddd825efcdebded
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:39:54 +0200

    Add files via upload

---
+++ Data/Models/AuthorBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.Data.Models
+{
+    public class AuthorBook
+    {
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
---
+++ DataProcessor/ImportDto/BookInputModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using BookShop.Data.Models.Enums;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    [XmlType("Book")]
+    public class BookInputModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [EnumDataType(typeof(Genre))]
+        [XmlElement("Genre")]
+        public int Genre { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+        [Range(50, 5000)]
+        [XmlElement("Pages")]
+        public int Pages { get; set; }
+
+        [Required]
+        [XmlElement("PublishedOn")]
+        public string PublishedOn { get; set; }
+    }
+}
