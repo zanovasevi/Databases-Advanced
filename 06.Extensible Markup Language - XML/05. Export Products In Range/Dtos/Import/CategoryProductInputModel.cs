a6acac4ff31cc981a6fc3450f49ee210e972ef1b
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:18:42 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/05. Export Products In Range/Dtos/Import/CategoryProductInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType("CategoryProduct")]
+    public class CategoryProductInputModel
+    {
+        [XmlElement("CategoryId")]
+        public int CategoryId { get; set; }
+
+        [XmlElement("ProductId")]
+        public int ProductId { get; set; }
+    }
+}
+//< CategoryProduct >
+//        < CategoryId > 4 </ CategoryId >
+//        < ProductId > 1 </ ProductId >
+//</ CategoryProduct >
