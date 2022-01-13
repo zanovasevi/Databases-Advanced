2136e484556dc287bd5f096d2eaf17c6ceeb8a6b
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:29:05 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/16. Export Local Suppliers/DTO/Output/SupplierOutputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Output
+{
+    [XmlType("suplier")]
+    public class SupplierOutputModel
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("parts-count")]
+        public int PartsCount { get; set; }
+    }
+}
