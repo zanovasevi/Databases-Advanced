774791dbc0a93e882f8b4b9c9a890e98e0dcfac8
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:06:50 +0200

    Add files via upload

---
+++ 05.JavaScript Object Notation - JSON/13. Import Sales/DTO/CarInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using CarDealer.Models;
+
+namespace CarDealer.DTO
+{
+    public class CarInputModel
+    {
+        public CarInputModel()
+        {
+            PartsId = new HashSet<int>();
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        public IEnumerable<int> PartsId { get; set; }
+
+    }
+}
