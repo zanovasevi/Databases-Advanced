d4dbe0fad5be3151366b506da5279029a9a29d54
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 14:27:07 +0200

    Add files via upload

---
+++ 02.Entity Relations/1. Student System/Data/StudentSystemContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost; User Id=sa; Password=reallyStrongPwd123; Database=StudentSystem");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.StudentId, x.CourseId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
