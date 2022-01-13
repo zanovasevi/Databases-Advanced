c22146d97e92ae0859aba20570c4ce09d24601a8
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:02:31 +0200

    Create StartUp.cs

---
+++ 05.JavaScript Object Notation - JSON/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string supplierJson = File.ReadAllText("../../../Datasets/suppliers.json");
+
+            var result = ImportSuppliers(context, supplierJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoSuppliers = JsonConvert.DeserializeObject<IEnumerable<SupplierInputModel>>(inputJson);
+
+            var suppliers = mapper.Map<IEnumerable<Supplier>>(dtoSuppliers);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+
+            mapper = config.CreateMapper();
+        }
+    }
+}
