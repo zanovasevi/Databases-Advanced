636786a7f30a7e83206fa1b825393c18cdee61aa
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:32:05 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTO/Output/CustomerOutputModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Output
+{
+    [XmlType("customer")]
+    public class CustomerOutputModel
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public decimal SpentMoney { get; set; }
+    }
+}
+//< customers >
+
+//     < customer full - name = "Hai Everton" bought - cars = "1" spent - money = "2544.67" />
+         
+//     < customer full - name = "Daniele Zarate" bought - cars = "1" spent - money = "2014.83" />
+                  
+//     < customer full - name = "Donneta Soliz" bought - cars = "1" spent - money = "1655.57" />
+
+//</ customers >
---
+++ 06.Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTO/Output/SaleOutputModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Output
+{
+    [XmlType("sale")]
+    public class SaleOutputModel
+    {
+        [XmlElement("car")]
+        public CarSaleOutputModel Car { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public decimal PriceWithDiscount { get; set; }
+    }
+}
+//< sale >
+//    < car make = "BMW" model = "M5 F10" travelled - distance = "435603343" />
+     
+//         < discount > 30.00 </ discount >
+     
+//         < customer - name > Hipolito Lamoreaux </ customer - name >
+          
+//         < price > 707.97 </ price >
+          
+//         < price - with - discount > 495.58 </ price - with - discount >
+          
+//</ sale >
