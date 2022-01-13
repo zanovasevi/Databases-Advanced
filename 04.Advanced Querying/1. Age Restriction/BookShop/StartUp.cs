73537c76fec6303a8a26c508ab452c80d7243295
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 14:40:54 +0200

    Add files via upload

---
+++ 04.Advanced Querying/1. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Linq;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(books => books.AgeRestriction == ageRestriction)
+                .Select(book => book.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            var result = string.Join(Environment.NewLine, books);
+
+            return result;
+        }
+    }
+}
