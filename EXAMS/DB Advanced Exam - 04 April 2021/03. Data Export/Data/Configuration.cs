0e5c90ae22d96f0c22c8e0faa060155af4c352d9
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:53:54 +0200

    Add files via upload

---
+++ EXAMS/DB Advanced Exam - 04 April 2021/03. Data Export/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=localhost;Database=TeisterMask2;User Id=sa;Password=reallyStrongPwd123";
+    }
+}
---
+++ EXAMS/DB Advanced Exam - 04 April 2021/03. Data Export/DataProcessor/ImportDto/ProjectTaskInputModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Project")]
+    public class ProjectTaskInputModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+        public TaskInputModel[] Tasks { get; set; }
+    }
+
+    [XmlType("Task")]
+    public class TaskInputModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; }
+
+        [Required]
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+        [EnumDataType(typeof(ExecutionType))]
+        [XmlElement("ExecutionType")]
+        public int ExecutionType { get; set; }
+
+        [EnumDataType(typeof(LabelType))]
+        [XmlElement("LabelType")]
+        public int LabelType { get; set; }
+    }
+}
