a1de44ada0a5d4e2bd4190bd3d6889cb83d2752e
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:12:32 +0200

    Add files via upload

---
+++ 05.JavaScript Object Notation - JSON/19. Export Sales With Applied Discount/DTO/SalesInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CarDealer.DTO
+{
+    public class SalesInputModel
+    {
+        public decimal Discount { get; set; }
+
+        public int CarId { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
