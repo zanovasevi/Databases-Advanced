1aba8ae898d08a830b31b29c0d4753e6e2e0deb3
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:30:07 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/17. Export Cars With Their List Of Parts/DTO/Input/SaleInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Input
+{
+    [XmlType("Sale")]
+    public class SaleInputModel
+    {
+        [XmlElement("carId")]
+        public int CarId { get; set; }
+
+        [XmlElement("customerId")]
+        public int CustomerId { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+    }
+}
+//< Sale >
+//        < carId > 105 </ carId >
+//        < customerId > 30 </ customerId >
+//        < discount > 30 </ discount >
+//</ Sale >
