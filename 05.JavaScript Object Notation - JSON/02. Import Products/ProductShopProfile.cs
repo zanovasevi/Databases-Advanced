4b43b05e60f8e9e5313222e0cc0132c929bcf35c
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 14:55:33 +0200

    Add files via upload

---
+++ 05.JavaScript Object Notation - JSON/02. Import Products/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductShop.IO.Input;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputModel, User>();
+            CreateMap<ProductInputModel, Product>();
+        }
+    }
+}
