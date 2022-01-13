564927882375c81a76f4b92e76750a04fdc5f528
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:16:45 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/03. Import Categories/Dtos/Import/CategoryInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType("Category")]
+    public class CategoryInputModel
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+}
