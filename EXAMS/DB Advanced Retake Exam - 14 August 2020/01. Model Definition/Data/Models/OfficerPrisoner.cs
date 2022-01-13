e0125fca6953ad39305d6da3cdef715cdb22bdb6
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:42:03 +0200

    Add files via upload

---
+++ EXAMS/DB Advanced Retake Exam - 14 August 2020/01. Model Definition/Data/Models/OfficerPrisoner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftJail.Data.Models
+{
+    public class OfficerPrisoner
+    {
+        public int PrisonerId { get; set; }
+
+        public Prisoner Prisoner { get; set; }
+
+        public int OfficerId { get; set; }
+
+        public Officer Officer { get; set; }
+    }
+}
