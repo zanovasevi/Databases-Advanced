dc59ac2c4c26af9d45be4912293ea5e8245862e3
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:42:39 +0200

    Delete Data directory

--- Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookShop.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=localhost;Database=BookShop2;User Id=sa;Password=reallyStrongPwd123";
-    }
-}
