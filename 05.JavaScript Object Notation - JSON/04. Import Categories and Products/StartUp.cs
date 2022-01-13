49e6b641f7e6ff25234fc574a2958916c47e185d
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 14:57:21 +0200

    Create StartUp.cs

---
+++ 05.JavaScript Object Notation - JSON/04. Import Categories and Products/StartUp.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.IO.Input;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string userJson = File.ReadAllText("../../../Datasets/users.json");
+            string productJson = File.ReadAllText("../../../Datasets/products.json");
+            string categoryJson = File.ReadAllText("../../../Datasets/categories.json");
+            string categoryProductJson = File.ReadAllText("../../../Datasets/categories-products.json");
+
+            ImportUsers(context, userJson);
+            ImportProducts(context, productJson);
+            ImportCategories(context, categoryJson);
+
+            var result = ImportCategoryProducts(context, categoryProductJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoCategoryProducts = JsonConvert.DeserializeObject<IEnumerable<CategoryProductInputModel>>(inputJson);
+
+            var categoryProducts = mapper.Map<IEnumerable<CategoryProduct>>(dtoCategoryProducts);
+
+            context.CategoryProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {categoryProducts.Count()}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoCategories = JsonConvert.DeserializeObject<IEnumerable<CategoryInputModel>>(inputJson);
+
+            var categories = mapper.Map<IEnumerable<Category>>(dtoCategories)
+                .Where(x => x.Name != null)
+                .ToList();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count()}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoProducts = JsonConvert.DeserializeObject<IEnumerable<ProductInputModel>>(inputJson);
+
+            var products = mapper.Map<IEnumerable<Product>>(dtoProducts);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count()}";
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoUsers = JsonConvert.DeserializeObject<IEnumerable<UserInputModel>>(inputJson);
+
+            var users = mapper.Map<IEnumerable<User>>(dtoUsers);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = config.CreateMapper();
+        }
+    }
+}
