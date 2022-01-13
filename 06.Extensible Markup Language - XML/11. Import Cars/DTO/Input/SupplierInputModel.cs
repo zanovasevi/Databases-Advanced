48ece34a5da1349892989656b22ba94b092ff1f3
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:23:59 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/11. Import Cars/DTO/Input/SupplierInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Input
+{
+    [XmlType("Supplier")]
+    public class SupplierInputModel
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
