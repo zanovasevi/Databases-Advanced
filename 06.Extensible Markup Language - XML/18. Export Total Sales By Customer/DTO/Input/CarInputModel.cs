51a0af889fe8ad180d5d41b00a67797aeddca30a
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 15:31:07 +0200

    Add files via upload

---
+++ 06.Extensible Markup Language - XML/18. Export Total Sales By Customer/DTO/Input/CarInputModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using CarDealer.Models;
+
+namespace CarDealer.DTO.Input
+{
+    [XmlType("Car")]
+    public class CarInputModel
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("TravelledDistance")]
+        public long TravelledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public CarPartsInputModel[] CarPartsInputModel { get; set; }
+    }
+}
+//< Car >
+//    < make > Opel </ make >
+//    < model > Astra </ model >
+//    < TraveledDistance > 516628215 </ TraveledDistance >
+//    < parts >
+//      < partId id = "39" />
+ 
+//       < partId id = "62" />
+  
+//        < partId id = "72" />
+   
+//   </ parts >
+   
+//</ Car >
